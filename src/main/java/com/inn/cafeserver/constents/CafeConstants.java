82d5cb92f22f1c1575392abdd49dcd9b238d83c2
Jahuruddin SK <Jahuruddin@BCTCL-DL5420552.bctchn.local>
2023-08-10 22:35:37 +0530

    Full backend part implementalion is done for cafe management system

---
+++ src/main/java/com/inn/cafeserver/constents/CafeConstants.java
@@ -0,0 +1,12 @@
+package com.inn.cafeserver.constents;
+
+public class CafeConstants {
+    public static final String SOMETHING_WENT_WRONG = "Something Went Wrong!!.";
+
+    public static final String INVALID_DATA = "Invalid Data !!.";
+
+    public static final String UNAUTHORIZED_ACCESS = "Unauthorized Access!,";
+
+    public static final String STORE_LOCATION = "C:\\CMS";
+
+}
